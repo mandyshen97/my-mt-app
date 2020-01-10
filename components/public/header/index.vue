268b05a3d52f21fffa5f67c141e85aa63d967d79
mandyshen <1099232082@qq.com>
2020-01-10 13:41:25 +0800

    添加模板layout/default.vue结构

---
+++ components/public/header/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <top-bar />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>searchbar</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import topBar from "./topbar";
+export default {
+  components: {
+    topBar
+  }
+};
+</script>
+
+<style lang="css"></style>
---
+++ components/public/header/topbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-row :gutter="0" class="m-header">
+    <el-col :span="5">wewerfewgfggfd1</el-col>
+    <el-col :span="5">2</el-col>
+    <el-col :span="14">3</el-col>
+  </el-row>
+  </template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+@import "@/assets/css/public/layout.scss";
+@import "@/assets/css/public/header/index.scss";
+
+</style>
