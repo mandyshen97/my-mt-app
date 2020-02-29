ceef563d1dbfef39bbecec6fd4a8aa58d65c5044
mandyshen <1099232082@qq.com>
2020-02-29 22:11:24 +0800

    citys

---
+++ server/dbs/models/city.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongose'
+const Schema = mongoose.Schema
+const City = new Schema({
+  id:{
+    type:String,
+    required: true
+  },
+  value:{
+    type:Array,
+    required:true
+  }
+})
+
+export default mongoose.model('City',City)
